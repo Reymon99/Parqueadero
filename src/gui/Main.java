d681c6cca7427dddb46feaba0f05619bc912bd10
s.maje <s.maje@udla.edu.co>
2019-12-03 11:19:18 -0500

    Definición del Frame

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,7 +1,20 @@
 package gui;
 import javax.swing.*;
+import java.awt.*;
 public class Main extends JFrame {
+    public Main(){
+        super("Parqueadero");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new GridBagLayout());
+        init();
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
     public static void main(String[] args) {
         new Main().setVisible(true);
+    }
+    private void init(){
+
     }
 }
