58744fe1030505cf75a7dc23ada4feb96752480e
s.maje <s.maje@udla.edu.co>
2019-12-12 04:26:39 -0500

    seis espacios de estacionamiento

--- src/gui/Parqueadero.java
+++ src/gui/Parqueadero.java
@@ -46,10 +46,26 @@ public class Parqueadero extends JPanel {
     }
     private void lines(Graphics2D g2){
         g2.setColor(new Color(219, 210, 11));
-        g2.setStroke(new BasicStroke(4.0f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10.0f, new float[]{25.0f, 25.0f}, 0.0f));
-        g2.drawLine(pointWidth(11), pointHeight(50) - 2, minusWidth(pointWidth(11)), pointHeight(50) - 2);
+        g2.setStroke(new BasicStroke(5.0f, BasicStroke.CAP_SQUARE, BasicStroke.JOIN_MITER, 10.0f, new float[]{25.0f, 25.0f}, 0.0f));
+        g2.drawLine(pointWidth(10), pointHeight(50) - 2, minusWidth(pointWidth(10)), pointHeight(50) - 2);
     }
     private void parkingSpaces(Graphics2D g2){
+        g2.setColor(new Color(130, 137, 143));
+        g2.setStroke(new BasicStroke(19.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);//Delimita perfectamente los pixeles curvos
+        int[] ejesX = new int[]{pointWidth(19), pointWidth(19) + 210, pointWidth(19) + 210 * 2};
+        for (int x = 0; x < ejesX.length; x++){
+            parkingSpace(g2, ejesX[x], 0);
+            parkingSpace(g2, ejesX[x], minusHeight(120));
+            number(g2, x, ejesX[x], 0);
+            number(g2, x + 3, ejesX[x], minusHeight(120));
+        }
+    }
+    private void parkingSpace(Graphics2D g2, int x, int y){
+        g2.drawLine(x, y, x, y + 120);
+        g2.drawLine(x + 170, y, x + 170, y + 120);
+    }
+    private void number(Graphics2D g2, int number, int x, int y){
 
     }
     @Override
