0e0a71fed7d9a864d4f46329f462fc157a13375a
s.maje <s.maje@udla.edu.co>
2019-12-16 01:30:01 -0500

    panel de control

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -16,5 +16,14 @@ public class Main extends JFrame {
     }
     private void init(){
         Constrains.addComp(new Parqueadero(), this, new Rectangle(0, 0, 1, 1), 0, 0, new Insets(0, 0, 0, 0), GridBagConstraints.CENTER, GridBagConstraints.NONE);
+        Constrains.addCompX(panelDeControl(), this, new Rectangle(0, 1, 1, 1), 0.3, new Insets(10, 5, 5, 5), GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL);
+    }
+    private JPanel panelDeControl(){
+        final JPanel panel = new JPanel(new GridBagLayout());
+        JButton start = new JButton("Iniciar");
+        JButton end = new JButton("Terminar");
+        Constrains.addCompX(start, panel, new Rectangle(0, 0, 1, 1), 1, new Insets(15, 15, 15, 10), GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL);
+        Constrains.addCompX(end, panel, new Rectangle(1, 0, 1, 1), 1, new Insets(15, 15, 15, 15), GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL);
+        return panel;
     }
 }
