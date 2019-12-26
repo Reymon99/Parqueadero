767ad0b403ca42e94e53573f94e18ca1983ab502
s.maje <s.maje@udla.edu.co>
2019-12-26 11:47:26 -0500

    models

--- src/gui/PanelControl.java
+++ src/gui/PanelControl.java
@@ -6,14 +6,16 @@ import java.awt.*;
 import java.util.LinkedList;
 public class PanelControl extends JPanel {
     public static boolean activo;
-    private JTextArea enLista;
-    private JTextArea notificaciones;
+    private final DefaultListModel<String> enEspera;
+    private final DefaultListModel<String> notificaciones;
     private JLabel carrosFaltantes;
     private JLabel carrosParqueados;
     private JLabel carrosDespachados;//el se del contador solo anidar el número al mensaje preescrito
     private final LinkedList<Carro> carrosOut;
     private final Carro[] carrosIn;
     {
+        enEspera = new DefaultListModel<>();
+        notificaciones = new DefaultListModel<>();
         carrosOut = new LinkedList<>();
         carrosIn = new Carro[6];
     }
@@ -32,17 +34,22 @@ public class PanelControl extends JPanel {
             //lanzar hilo principal acá, que a la vez ese hilo lanzará los multiples hilos que controlarán los automoviles en el estacionamiento
         });
         end.addActionListener((e) -> juego());
+        final JList<String> espera = new JList<>(enEspera);
+        final JList<String> notify = new JList<>(notificaciones);
+        espera.setBorder(BorderFactory.createTitledBorder("En Espera"));
+        notify.setBorder(BorderFactory.createTitledBorder("Notificaciones"));
         Constrains.addCompX(start, this, new Rectangle(0, 0, 1, 1), 1, new Insets(15, 15, 15, 10), GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL);
-        Constrains.addCompX(end, this, new Rectangle(0, 1, 1, 1), 1, new Insets(15, 15, 15, 15), GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL);
+        Constrains.addCompX(end, this, new Rectangle(1, 0, 1, 1), 1, new Insets(15, 15, 15, 15), GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL);
     }
     private void juego(){
         PanelControl.activo = !PanelControl.activo;
     }
     private void addCar(Carro carro) throws NullPointerException {
         if (carro == null) throw new NullPointerException("No se encuentra carro a añadir");
-        else if (enLista == null || carrosOut == null) throw new NullPointerException("No existe lista para el parqueadero");
+        else if (enEspera == null || carrosOut == null) throw new NullPointerException("No existe lista para el parqueadero");
         else {
-            enLista.insert(carro.toString() + " esperando\n", 0);
+            enEspera.add(0, carro.toString() + " esperando");
+            notificaciones.add(0, carro.toString() + " en espera");
             carrosOut.add(carro);
         }
     }
